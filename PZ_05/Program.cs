d6885722649c75105260132c8d99dac690db1a6c
Иван Кузнецов <zabivaka1234@outlook.com>
2023-09-22 23:05:05 +0300

    Добавил практику 5

---
+++ PZ_05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = -2;
+            double  y;
+            double step = 0.5;
+            
+            while (x <= 2)
+            {                
+                y = -2.4*Math.Pow(x, 2)+ 5*x - 3;                
+                Console.WriteLine($"значение X:{x}"  + " " + $"значение y:{Math.Round(y)}");                            
+                x += step; 
+                
+            }
+        }
+
+
+
+
+    }
+}
