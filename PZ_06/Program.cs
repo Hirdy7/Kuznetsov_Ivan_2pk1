9787fd487de1b79e427a88a3228319150cc2a746
Иван Кузнецов <zabivaka1234@outlook.com>
2023-09-27 15:39:58 +0300

    практика 6

---
+++ PZ_06/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PZ_06
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[10];
+            Random rand = new Random();
+
+            // Заполняем массив случайными числами в интервале [-10..10]
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-10, 11);
+            }
+
+            // Реверсируем все элементы, кроме последнего
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - i - 2];
+                array[array.Length - i - 2] = temp;
+            }
+
+            // Выводим массив на экран
+            Console.WriteLine("Массив после реверса:");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
