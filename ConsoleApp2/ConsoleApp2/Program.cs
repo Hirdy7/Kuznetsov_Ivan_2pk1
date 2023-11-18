e082bcb738c9a0ee51b2e2cad18bb48bc63c0dd6
Иван Кузнецов <zabivaka1234@outlook.com>
2023-11-18 20:14:18 +0300

    пратика 9

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+//Console.WriteLine(" первое и второе задание");
+//Random random = new Random();
+//string[][] array = new string[9][];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    for (int j = 0; j < 9; j++)
+//    {
+//        array[j] = new string[random.Next(6, 16)];
+//    }
+//}
+
+//String rndstr = ("qwertyuiopasdfghjklzxcvbnm");
+//for (int i = 0; i < array.Length; i++)
+//{
+//    for (int j = 0; j < array[i].Length; j++)
+
+//    {
+//        array[i][j] = Convert.ToString(rndstr[random.Next(rndstr.Length)]);
+//        Console.Write(array[i][j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("задание 3");
+
+// string[] lastelement = new string[15];
+//Array.Reverse(array);
+//Console.WriteLine(array);
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание случайного числа для второго измерения
+        Random random = new Random();
+        int secondDimension = random.Next(7, 16);
+
+        // Создание двумерного массива строк
+        string[,] twoDimensionalArray = new string[9, secondDimension];
+
+        // Заполнение двумерного массива случайными строками
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < secondDimension; j++)
+            {
+                twoDimensionalArray[i, j] = GenerateRandomString();
+            }
+        }
+
+        // Создание нового одномерного массива и запись в него последних элементов каждой строки
+        string[] newArray = new string[9];
+        for (int i = 0; i < 9; i++)
+        {
+            newArray[i] = twoDimensionalArray[i, secondDimension - 1];
+        }
+
+        // Вывод нового одномерного массива
+        Console.WriteLine("Результат:");
+        foreach (string element in newArray)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.ReadLine();
+    }
+
+    // Метод для генерации случайной строки
+    static string GenerateRandomString()
+    {
+        const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        char[] stringArray = new char[10];
+        for (int i = 0; i < 10; i++)
+        {
+            stringArray[i] = characters[random.Next(characters.Length)];
+        }
+        Console.WriteLine(characters);
+        return new string(stringArray);
+        
+    }
+    
+}
---
+++ PZ_09/PZ_09/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace PZ_09
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Введите любую строку:");
+            string str = Console.ReadLine();
+            str = str.Replace(" ", "");
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+            
+                if (i != str.Length - 1)
+                {
+                     
+                    if (str[i] != str[i + 1])
+                        Console.Write(str[i]);
+                        
+                }
+                
+            }
+            Console.Write(str[str.Length - 1]);
+           
+
+
+
+
+        }
+
+    }
+}
