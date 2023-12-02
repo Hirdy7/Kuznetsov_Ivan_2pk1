8832c566865aafd2d7e1911985fcb7c27c3de9d3
Hirdy7 <143997679+Hirdy7@users.noreply.github.com>
2023-12-02 14:33:53 +0300

    Program.cs

--- PZ_11/Program.cs
+++ PZ_11/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace PZ_11
+using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace PZ_11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            First(ref a, ref b);
+            Second(a, b, ref c);
+            Third(c);
+
         }
+        static void First(ref int a, ref int b)
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+        }
+        static void Second(int a, int b, ref int c)
+        {
+            c = a + b;
+
+        }
+        static void Third(int c)
+        {
+            Console.WriteLine(c);
+        }
+
+
+
     }
 }
