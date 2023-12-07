4f84bded48db7aec400d73c15af3cc1805ab4550
Иван Кузнецов <zabivaka1234@outlook.com>
2023-12-07 21:01:05 +0500

    практика 15

---
+++ PZ_15/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_15
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string directoryPath = Console.ReadLine();
+
+            string sourcePath = Path.Combine(directoryPath, "full");
+            string destinationPath = Path.Combine(directoryPath, "empty");
+
+            if (Directory.Exists(sourcePath) && Directory.Exists(destinationPath))
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string fileName = Path.GetFileName(files[i]);
+                    string destinationFile = Path.Combine(destinationPath, fileName);
+                    File.Move(files[i], destinationFile);
+                    Console.WriteLine("Перенос файла {0} выполнен успешно", fileName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Каталоги full и/или empty не существуют.");
+            }
+
+            Console.ReadLine();
+        }
+    }
